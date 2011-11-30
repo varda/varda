55cd0b922e44b4b2ea4f1cfcca089490c4731232
Martijn Vermaat <martijn@vermaat.name>
2011-11-30 17:01:26 +0100

    Implement error handling

--- varda/tasks.py
+++ varda/tasks.py
@@ -4,6 +4,7 @@ Celery tasks.
 
 
 import os
+from contextlib import contextmanager
 
 from sqlalchemy.exc import IntegrityError
 
@@ -12,7 +13,80 @@ from varda.models import Variant, Sample, Observation, DataSource
 
 
 class TaskError(Exception):
-    pass
+    """
+    Exception thrown on failed task execution.
+    """
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+        super(Exception, self).__init__(code, message)
+
+    def to_dict(self):
+        return {'code':    self.code,
+                'message': self.message}
+
+
+@contextmanager
+def database_task(cleanup=None):
+    """
+    Context manager for a Celery task using the database.
+
+    Upon closing, the database session is committed and if a TaskError was
+    raised, the cleanup function argument is called first.
+
+    Todo: We might add a setup function as argument.
+    """
+    try:
+        yield
+    except TaskError:
+        if cleanup is not None:
+            cleanup()
+        raise
+    finally:
+        db.session.commit()
+
+
+@celery.task
+def import_bed(sample_id, data_source_id):
+    """
+    Import regions from BED file.
+    """
+    data_source = DataSource.query.get(data_source_id)
+    if not data_source:
+        raise TaskError('data_source_not_found', 'Data source not found')
+
+    sample = Sample.query.get(sample_id)
+    if not sample:
+        raise TaskError('sample_not_found', 'Sample not found')
+
+    # Todo: SQLAlchemy probably has something for this, has() or any() or exists()...
+    if sample.regions.filter_by(data_source=data_source).count() > 1:
+        raise TaskError('data_source_imported', 'Data source already imported in this sample')
+
+    bed_file = os.path.join(app.config['FILES_DIR'], data_source.filename)
+
+    # Note: Since we are dealing with huge numbers of entries here, we
+    # commit after each INSERT and manually rollback. Using builtin
+    # session rollback would fill up all our memory.
+    def delete_regions():
+        sample.regions.filter_by(data_source=data_source).delete()
+
+    # Todo: This (multiple context managers) is a Python 2.7 feature
+    with task_context(cleanup=delete_regions) as _, open(bed_file) as bed:
+
+        for line in bed:
+            fields = line.split()
+            if len(parts) < 1 or parts[0] == 'track':
+                continue
+            try:
+                chromosome = parts[0]
+                begin = int(parts[1])
+                end = int(parts[2])
+            except (IndexError, ValueError):
+                raise TaskError('data_source_invalid', 'Invalid line in BED file: "%s"' % line)
+            region = Region(sample, data_source, chromosome, begin, end)
+            db.session.add(region)
+            db.session.commit()
 
 
 @celery.task
@@ -21,85 +95,76 @@ def import_vcf(sample_id, data_source_id, use_genotypes=True):
     Import observed variants from VCF file.
 
     @todo: This only works for merged population studies at the moment.
-    @todo: Check if it has already been imported.
     @todo: Use custom state to report progress:
         http://docs.celeryproject.org/en/latest/userguide/tasks.html#custom-states
-
-    @note: Uncommitted writes to MySQL seem to be cached in memory by SQLAlchemy
-        since MySQL does not have commit/rollback functionality. The unpleasant
-        side-effect is that we need tons of memory if we only commit after the
-        entire VCF file is imported. So we commit after each variant.
-        Unfortunately, this looses the most obvious way to do error recovery by
-        doing a rollback on all variants imported thus far.
     """
     data_source = DataSource.query.get(data_source_id)
     if not data_source:
-        raise TaskError('Data source not found')
-
-    vcf_file = os.path.join(app.config['FILES_DIR'], data_source.filename)
-    vcf = open(vcf_file)
-
-    header = vcf.readline()
-    if 'fileformat=VCFv4.1' not in header:
-        raise TaskError('Data source not in VCF version 4.1 format')
+        raise TaskError('data_source_not_found', 'Data source not found')
 
     sample = Sample.query.get(sample_id)
     if not sample:
-        raise TaskError('Sample not found')
+        raise TaskError('sample_not_found', 'Sample not found')
 
     # Todo: SQLAlchemy probably has something for this, has() or any() or exists()...
-    if sample.observations.filter(data_source=data_source).count() > 1:
-        raise TaskError('Data source already imported in this sample')
+    if sample.observations.filter_by(data_source=data_source).count() > 1:
+        raise TaskError('data_source_imported', 'Data source already imported in this sample')
 
-    for line in vcf:
-        if line.startswith('#'):
-            continue
-        fields = line.split()
-        info = dict(field.split('=') if '=' in field else (field, None) for field in fields[7].split(';'))
-        chromosome, position, _, reference, variant = fields[:5]
-        if use_genotypes:
-            genotypes = [genotype.split(':')[0] for genotype in fields[9:]]
-        for index, allele in enumerate(variant.split(',')):
-            if 'SV' in info:
-                # SV deletion (in 1KG)
-                # Todo: For now we ignore these, reference is likely to be
-                # larger than the maximum of 200 by the database schema.
-                #end = int(position) + len(reference) - 1
-                #allele = ''
+    vcf_file = os.path.join(app.config['FILES_DIR'], data_source.filename)
+
+    # Note: Since we are dealing with huge numbers of entries here, we
+    # commit after each INSERT and manually rollback. Using builtin
+    # session rollback would fill up all our memory.
+    def delete_observations():
+        sample.observations.filter_by(data_source=data_source).delete()
+
+    # Todo: This (multiple context managers) is a Python 2.7 feature
+    with task_context(cleanup=delete_observations), open(vcf_file) as vcf:
+
+        header = vcf.readline()
+        if 'fileformat=VCFv4.1' not in header:
+            raise TaskError('data_source_invalid', 'Data source not in VCF version 4.1 format')
+
+        for line in vcf:
+            if line.startswith('#'):
                 continue
-            elif ('SVTYPE' in info and info['SVTYPE'] == 'DEL') or \
-                 ('INDEL' in info and len(reference) >= len(allele)):
-                # Deletion
-                end = int(position) + len(reference) - 1
-            else:
-                # SNP or insertion.
-                end = position
-            variant = Variant.query.filter_by(chromosome=chromosome, begin=position, end=end, reference=reference, variant=allele).first()
-            if not variant:
-                variant = Variant(chromosome, position, end, reference, allele)
-                db.session.add(variant)
-                db.session.commit()
+            fields = line.split()
+            info = dict(field.split('=') if '=' in field else (field, None) for field in fields[7].split(';'))
+            chromosome, position, _, reference, variant = fields[:5]
             if use_genotypes:
-                support = sum(1 for genotype in genotypes if str(index + 1) in genotype)
-            elif 'SF' in info:
-                support = len(info['SF'].split(','))
-            elif 'AC' in info:
-                support = int(info['AC'])
-            else:
-                raise TaskError('Cannot read variant support')
-            try:
-                observation = Observation(sample, variant, data_source, support=support)
-            except IntegrityError:
-                raise TaskError('Observation already exists')
-            db.session.add(observation)
-            db.session.commit()
-    vcf.close()
-
-
-# Testing tasks
-@celery.task
-def add_variant(chromosome, begin, end, reference, variant):
-    variant = Variant(chromosome, begin, end, reference, variant)
-    db.session.add(variant)
-    db.session.commit()
-    return variant.id
+                genotypes = [genotype.split(':')[0] for genotype in fields[9:]]
+            for index, allele in enumerate(variant.split(',')):
+                if 'SV' in info:
+                    # SV deletion (in 1KG)
+                    # Todo: For now we ignore these, reference is likely to be
+                    # larger than the maximum of 200 by the database schema.
+                    #end = int(position) + len(reference) - 1
+                    #allele = ''
+                    continue
+                elif ('SVTYPE' in info and info['SVTYPE'] == 'DEL') or \
+                     ('INDEL' in info and len(reference) >= len(allele)):
+                    # Deletion
+                    end = int(position) + len(reference) - 1
+                else:
+                    # SNP or insertion.
+                    end = position
+                variant = Variant.query.filter_by(chromosome=chromosome, begin=position, end=end, reference=reference, variant=allele).first()
+                if not variant:
+                    variant = Variant(chromosome, position, end, reference, allele)
+                    db.session.add(variant)
+                    db.session.commit()
+                if use_genotypes:
+                    support = sum(1 for genotype in genotypes if str(index + 1) in genotype)
+                elif 'SF' in info:
+                    support = len(info['SF'].split(','))
+                elif 'AC' in info:
+                    support = int(info['AC'])
+                else:
+                    raise TaskError('data_source_invalid', 'Cannot read variant support')
+                try:
+                    observation = Observation(sample, variant, data_source, support=support)
+                except IntegrityError:
+                    # This should never happen since we check this above.
+                    raise TaskError('data_source_imported', 'Observation already exists')
+                db.session.add(observation)
+                db.session.commit()
--- varda/views.py
+++ varda/views.py
@@ -1,18 +1,60 @@
 """
 REST server views.
+
+Todo: For POST requests, we currently issue a 302 redirect to the view url of
+    the created object. An alternative would be to issue 200 success on object
+    creation and include the object identifier in a json response body.
+    Also, our 302 redirection pages are not json but HTML.
 """
 
 
 import os
 import uuid
 
-from flask import abort, request, redirect, url_for, jsonify
+from flask import abort, request, redirect, url_for, json
 from celery.exceptions import TimeoutError
 
 import varda
 from varda import app, db
 from varda.models import Variant, Sample, Observation, DataSource
-from varda.tasks import TaskError, import_vcf
+from varda.tasks import TaskError, import_vcf, import_bed
+
+
+def jsonify(_status=None, *args, **kwargs):
+    """
+    This is a temporary reimplementation of flask.jsonify that accepts a
+    special keyword argument '_status' for the HTTP response status code.
+
+    Eventually this will probably be implemented in Flask and we can use
+    something like
+
+        >>> @app.route('/')
+        >>> def my_view():
+        >>>     return flask.jsonify, 404
+
+    See also: https://github.com/mitsuhiko/flask/pull/239
+    """
+    return app.response_class(json.dumps(dict(*args, **kwargs), indent=None if request.is_xhr else 2),
+                              mimetype='application/json', status=_status)
+
+
+@app.errorhandler(400)
+def error_not_found(error):
+    return jsonify(error={'code': 'bad_request',
+                          'message': 'The request could not be understood due to malformed syntax'},
+                   _status=400)
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    return jsonify(error={'code': 'not_found',
+                          'message': 'The requested entity could not be found'},
+                   _status=404)
+
+
+@app.errorhandler(TaskError)
+def error_task_error(error):
+    return jsonify(error=error.to_dict(), _status=500)
 
 
 @app.route('/')
@@ -30,12 +72,12 @@ def samples_list():
     return jsonify(samples=[s.to_dict() for s in Sample.query])
 
 
-@app.route('/samples/<id>', methods=['GET'])
-def samples_get(id):
+@app.route('/samples/<sample_id>', methods=['GET'])
+def samples_get(sample_id):
     """
     curl -i http://127.0.0.1:5000/samples/2
     """
-    return jsonify(sample=Sample.query.get(id).to_dict())
+    return jsonify(sample=Sample.query.get_or_404(sample_id).to_dict())
 
 
 @app.route('/samples', methods=['POST'])
@@ -44,10 +86,34 @@ def samples_add():
     curl -i -d 'name=Genome of the Netherlands' -d 'pool_size=500' http://127.0.0.1:5000/samples
     """
     data = request.form
-    sample = Sample(data['name'], int(data['coverage_threshold']), int(data['pool_size']))
+    try:
+        name = data['name']
+        coverage_threshold = int(data.get('coverage_threshold', 8))
+        pool_size = int(data.get('pool_size', 1))
+    except (IndexError, ValueError):
+        abort(400)
+    sample = Sample(name, coverage_threshold, pool_size)
     db.session.add(sample)
     db.session.commit()
     return redirect(url_for('samples_get', id=sample.id))
+
+
+@app.route('/samples/<sample_id>/observations/<task_id>', methods=['GET'])
+def observations_get(sample_id, task_id):
+    """
+    Check status of import observations task.
+
+    Note: For a non-existing task_id, AsyncResult just returns a result with
+        status PENDING.
+    """
+    result = import_vcf.AsyncResult(task_id)
+    try:
+        # This re-raises a possible TaskError, handled by the error_task_error
+        # errorhandler above.
+        result.get(timeout=3)
+    except TimeoutError:
+        pass
+    return jsonify(observations={'task_id': task_id, 'ready': result.ready()})
 
 
 @app.route('/samples/<sample_id>/observations', methods=['POST'])
@@ -56,24 +122,45 @@ def observations_add(sample_id):
     curl -i -d 'data_source=3' http://127.0.0.1:5000/samples/1/observations
     """
     data = request.form
-    result = import_vcf.delay(sample_id, data['data_source'])
-    # Todo: check if call could be scheduled by celery
+    try:
+        sample_id = int(sample_id)
+        data_source_id = int(data['data_source'])
+    except (IndexError, ValueError):
+        abort(400)
+    Sample.query.get_or_404(sample_id)
+    DataSource.query.get_or_404(data_source_id)
+    result = import_vcf.delay(sample_id, data_source_id)
     return redirect(url_for('observations_get', sample_id=sample_id, task_id=result.task_id))
 
 
-@app.route('/samples/<sample_id>/observations/<task_id>', methods=['GET'])
-def observations_get(sample_id, task_id):
+@app.route('/samples/<sample_id>/regions/<task_id>', methods=['GET'])
+def regions_get(sample_id, task_id):
     """
-    Check status of import observations task.
+    Check status of import regions task.
     """
-    result = import_vcf.AsyncResult(task_id)
+    result = import_bed.AsyncResult(task_id)
     try:
-        result.get(timeout=1)
-    except TaskError as e:
-        return jsonify(error=str(e))
+        # This re-raises a possible TaskError, handled by the error_task_error
+        # errorhandler above.
+        result.get(timeout=3)
     except TimeoutError:
         pass
-    return jsonify(observations={'task_id': task_id, 'status': result.status})
+    return jsonify(regions={'task_id': task_id, 'ready': result.ready()})
+
+
+@app.route('/samples/<sample_id>/regions', methods=['POST'])
+def regions_add(sample_id):
+    """
+    curl -i -d 'data_source=3' http://127.0.0.1:5000/samples/1/regions
+    """
+    data = request.form
+    try:
+        sample_id = int(sample_id)
+        data_source_id = int(data['data_source'])
+    except (IndexError, ValueError):
+        abort(400)
+    result = import_bed.delay(sample_id, data_source_id)
+    return redirect(url_for('regions_get', sample_id=sample_id, task_id=result.task_id))
 
 
 @app.route('/data_sources', methods=['GET'])
@@ -84,19 +171,12 @@ def data_sources_list():
     return jsonify(data_sources=[d.to_dict() for d in DataSource.query])
 
 
-@app.route('/data_sources/<id>', methods=['GET'])
-def data_sources_get(id):
+@app.route('/data_sources/<data_source_id>', methods=['GET'])
+def data_sources_get(data_source_id):
     """
     Get an uploaded file id.
     """
-    #try:
-    #    with open(os.path.join(app.config['FILES_DIR'], data_source.filename)) as file:
-    #        data = file.read()
-    #except IOError:
-    #    abort(404)
-    #data_dict = data_source.to_dict()
-    #data_dict['data'] = data
-    return jsonify(data_source=DataSource.query.get(id).to_dict())
+    return jsonify(data_source=DataSource.query.get_or_404(data_source_id).to_dict())
 
 
 @app.route('/data_sources', methods=['POST'])
@@ -104,34 +184,14 @@ def data_sources_add():
     """
     Upload VCF or BED file.
     """
-    data = request.files['data']
-    if data:
-        filename = str(uuid.uuid4())
-        data.save(os.path.join(app.config['FILES_DIR'], filename))
-        data_source = DataSource(request.form['name'], filename)
-        db.session.add(data_source)
-        db.session.commit()
-        return redirect(url_for('data_sources_get', id=data_source.id))
-
-
-#@app.route('/populations/<id>', methods=['POST'])
-#def merged_observations_add():
-#    data = request.form
-#    result = add_merged_observations.AsyncResult(id).get(timeout=1.0)
-#    # etc
-
-
-# Below is testing tasks
-
-
-#@app.route('/cvariants', methods=['POST'])
-#def cvariants_add():
-#    data = request.form
-#    result = add_variant.apply_async( [data[f] for f in ('chromosome', 'begin', 'end', 'reference', 'variant')] )
-#    return redirect(url_for('cview', id=result.task_id))
-
-
-#@app.route('/cview/<id>', methods=['GET'])
-#def cview(id):
-#    result = add_variant.AsyncResult(id).get(timeout=1.0)
-#    return jsonify(variant=result)
+    try:
+        name = request.form['name']
+        data = request.files['data']
+    except IndexError:
+        abort(400)
+    filename = str(uuid.uuid4())
+    data.save(os.path.join(app.config['FILES_DIR'], filename))
+    data_source = DataSource(name, filename)
+    db.session.add(data_source)
+    db.session.commit()
+    return redirect(url_for('data_sources_get', id=data_source.id))
