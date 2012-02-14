47030155f5e7d8f7156d4a0f1dd1d45461740383
Martijn Vermaat <martijn@vermaat.name>
2012-02-14 13:22:05 +0100

    Documentation update

--- varda/__init__.py
+++ varda/__init__.py
@@ -1,5 +1,5 @@
 """
-Varda, a variant database interface.
+Varda server, a database for genomic variants.
 
 Copyright (c) 2011-2012, Leiden University Medical Center <humgen@lumc.nl>
 Copyright (c) 2011-2012, Martijn Vermaat <martijn@vermaat.name>
