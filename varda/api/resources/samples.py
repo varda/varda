db312f715d9366ecf81923ec89e9741f3540707d
Martijn Vermaat <martijn@vermaat.name>
2013-01-22 22:54:15 +0100

    API docs for samples

--- varda/api/resources/samples.py
+++ varda/api/resources/samples.py
@@ -54,13 +54,197 @@ class SamplesResource(Resource):
                    'public': {'type': 'boolean'}}
 
     @classmethod
+    def list_view(cls, *args, **kwargs):
+        """
+        Get a collection of samples.
+
+        Requires one of the following:
+         * the `admin` role
+         * being the user specified in the `user` argument
+         * the `public` argument set to ``True``
+
+        :arg public: If set to ``True`` or ``False``, restrict the collection to
+            public or non-public samples, respectively.
+        :type public: boolean
+        :arg user: If set to the URI for a user, restrict the collection to
+            samples owned by this user.
+        :type user: string
+        :statuscode 200: Respond with a list of :ref:`sample <api_samples>`
+            objects as `samples`.
+
+        Example request:
+
+        .. sourcecode:: http
+
+            GET /samples?public=true HTTP/1.1
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "samples":
+                [
+                  {
+                    "uri": "/samples/3",
+                    "user_uri": "/users/1",
+                    "name": "1KG phase 1 release",
+                    "pool_size": 1092,
+                    "public": true,
+                    "added": "2012-11-23T10:55:12.776706"
+                  },
+                  {
+                    "uri": "/samples/4",
+                    "user_uri": "/users/1",
+                    "name": "GoNL SNP release 4",
+                    "pool_size": 769,
+                    "public": true,
+                    "added": "2012-11-23T10:55:13.776706"
+                  }
+                ]
+            }
+        """
+        return super(SamplesResource, cls).list_view(*args, **kwargs)
+
+    @classmethod
+    def get_view(cls, *args, **kwargs):
+        """
+        Get details for a sample.
+
+        Requires the `admin` role or being the owner of the requested sample.
+
+        :statuscode 200: Respond with a :ref:`sample <api_samples>` object as `sample`.
+
+        Example request:
+
+        .. sourcecode:: http
+
+            GET /samples/3 HTTP/1.1
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "sample":
+                {
+                  "uri": "/samples/3",
+                  "user_uri": "/users/1",
+                  "name": "1KG phase 1 release",
+                  "pool_size": 1092,
+                  "public": true,
+                  "added": "2012-11-23T10:55:12.776706"
+                }
+            }
+        """
+        return super(SamplesResource, cls).get_view(*args, **kwargs)
+
+    @classmethod
     def add_view(cls, **kwargs):
+        """
+        Create a sample
+
+        Requires the `admin` or `importer` role.
+
+        :arg name: Human readable name.
+        :type name: string
+        :arg pool_size: Number of individuals (default: ``1``).
+        :type pool_size: integer
+        :arg coverage_profile: Whether or not this sample has a coverage profile
+            (default: ``True``).
+        :type coverage_profile: boolean
+        :arg public: Whether or not this sample is public (default: ``False``).
+        :type public: boolean
+        :statuscode 201: Respond with a URI for the created sample as `sample_uri`.
+
+        Example request:
+
+        .. sourcecode:: http
+
+            POST /samples HTTP/1.1
+            Content-Type: application/json
+
+            {
+              "name": "1KG phase 1 release",
+              "pool_size": 1092,
+              "coverage_profile": false,
+              "public": true
+            }
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 201 Created
+            Location: https://example.com/samples/13
+            Content-Type: application/json
+
+            {
+              "samples_uri": "/samples/13"
+            }
+        """
         kwargs['user'] = g.user
         return super(SamplesResource, cls).add_view(**kwargs)
 
     # Todo: Document that active will be set to False.
     @classmethod
     def edit_view(cls, *args, **kwargs):
+        """
+        Update a sample.
+
+        Requires the `admin` role or being the owner of the requested sample.
+
+        :arg active: If set to ``True`` or ``False``, activate or de-activate the
+            sample, respectively.
+        :type active: boolean
+        :arg name: Human readable name.
+        :type name: string
+        :arg pool_size: Number of individuals (default: ``1``).
+        :type pool_size: integer
+        :arg coverage_profile: Whether or not this sample has a coverage profile
+            (default: ``True``).
+        :type coverage_profile: boolean
+        :arg public: Whether or not this sample is public (default: ``False``).
+        :type public: boolean
+        :statuscode 200: Respond with a :ref:`sample <api_samples>` object as
+            `sample`.
+
+        Example request:
+
+        .. sourcecode:: http
+
+            PATCH /samples/14 HTTP/1.1
+            Content-Type: application/json
+
+            {
+              "active": true
+            }
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "sample":
+                {
+                  "uri": "/samples/14",
+                  "user_uri": "/users/1",
+                  "name": "1KG phase 1 release",
+                  "pool_size": 1092,
+                  "public": true,
+                  "added": "2012-11-23T10:55:12.776706"
+                }
+            }
+        """
         if kwargs.get('active'):
             # Todo: Checks, e.g. if there are expected imported data sources
             # and no imports running at the moment. Also, number of coverage
