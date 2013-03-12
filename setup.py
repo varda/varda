fa42fdbcd87356531d86f2324c7c79505109d0b1
Martijn Vermaat <martijn@vermaat.name>
2013-03-12 16:15:42 +0100

    Add setup.py (yet untested)

---
+++ setup.py
@@ -0,0 +1,36 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (2, 7):
+    raise Exception('Varda requires Python 2.7 or higher.')
+
+# Todo: How does this play with pip freeze requirement files?
+requires = []
+
+import varda as distmeta
+
+setup(
+    name='varda',
+    version=distmeta.__version__,
+    description='A database for genomic variation frequencies',
+    long_description=distmeta.__doc__,
+    author=distmeta.__author__,
+    author_email=distmeta.__contact__,
+    url=distmeta.__homepage__,
+    license='MIT License',
+    platforms=['any'],
+    packages=['varda'],
+    requires=requires,
+    entry_points = {
+        'console_scripts': ['varda-manage = varda.manages:main']
+        },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        ],
+    keywords='bioinformatics'
+)
