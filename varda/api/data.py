bb84539827cf98fbbe17d68094f92f2bbab15605
Martijn Vermaat <martijn@vermaat.name>
2013-03-24 18:52:00 +0100

    Separate casting of input data from validation
    Casting of values from input data to specific types is now done separately
    before handing the result over to our Cerberus validator. This means we don't
    rely anymore on our own Cerberus branch and generally makes for better
    separation of concerns.

--- varda/api/data.py
+++ varda/api/data.py
@@ -13,7 +13,7 @@ import re
 import urllib
 
 from cerberus import ValidationError as CerberusValidationError, Validator
-from cerberus.errors import ERROR_BAD_TYPE, ERROR_ITEMS_LIST
+import cerberus.errors
 from flask import current_app, g, request
 
 from ..models import Annotation, Coverage, DataSource, Sample, User, Variation
@@ -22,25 +22,166 @@ from .utils import (annotation_by_uri, coverage_by_uri, data_source_by_uri,
                     sample_by_uri, user_by_uri, variation_by_uri)
 
 
-ERROR_UNALLOWED_VALUE = "unallowed value '%s' for field '%s'"
-ERROR_LABEL_CHARACTERS = "field '%s' of %s type must contain only uppercase alphanumeric characters"
+def cast(document, schema):
+    """
+    Cast certain values in a data document to specific types.
+
+    This enables us to transparently handle incoming values from a JSON parser
+    as well as from URL query strings and HTTP request bodies, where every
+    value is, well, a string.
+
+    In addition, we use it to instantiate models from resource URIs, such that
+    we can work with model instances directly in the API view functions.
+
+    The function is 'silent', in the way that it will try to cast values but
+    never complains on failure. Instead, it will just return the original
+    value. The idea being that the reason for failure will be dealt with
+    during validation.
+
+    :arg document: Arbitrarily nested dictionary-like object containing our
+        values.
+    :type document: dict
+    :arg schema: Cerberus validation schema.
+    :type schema: dict
+
+    :return: Copy of `document` with possible updated values that could be
+        cast. Note that `document` is not modified in any way, but sub-
+        structures may have been copied by reference in the return value.
+    :rtype: dict
+    """
+    casters = {'list':        _cast_list,
+               'dict':        _cast_dict,
+               'integer':     _cast_integer,
+               'boolean':     _cast_boolean,
+               'annotation':  _cast_annotation,
+               'coverage':    _cast_coverage,
+               'data_source': _cast_data_source,
+               'sample':      _cast_sample,
+               'user':        _cast_user,
+               'variant':     _cast_variant,
+               'variation':   _cast_variation}
+    return_document = {}
+    for field, value in document.items():
+        definition = schema.get(field)
+        if definition and isinstance(definition, dict):
+            caster = casters.get(definition.get('type'))
+            if caster:
+                return_document[field] = caster(value, definition)
+            else:
+                return_document[field] = value
+    return return_document
 
 
-# Todo: We currently hacked the Validator class a bit such that some type
-#     casting is done in the 'type' rules by modifying the document in-place.
-#     This isn't really how Cerberus works and there might be a better way of
-#     doing things.
-#     Note the ugly consequence of this is that we explicitely rely on our own
-#     branch of Cerberus where the type rule is always checked first (the
-#     'varda' branch on github.com/martijnvermaat/cerberus).
-#     Since this concerns a critical part of our security, we should at least
-#     have some assertion somewhere checking that we are using this Cerberus
-#     branch (and ideally get rid of this setup alltogether).
-# Todo: I would like to have the app as extra argument to the constructor, so
-#     that it can be given to the `*_by_uri` functions and they would no
-#     longer need the ugly `current_app`. Unfortunately, that's not so easy
-#     since `Validator` methods instantiate new objects which would miss the
-#     argument.
+def _cast_list(values, definition):
+    if isinstance(values, basestring):
+        if values:
+            values = values.split(',')
+        else:
+            values = []
+    if isinstance(values, list):
+        schema = definition.get('schema')
+        if schema and isinstance(schema, dict):
+            return [cast({'key': value}, {'key': schema})['key']
+                    for value in values]
+        items = definition.get('items')
+        if items and isinstance(items, list) and len(items) == len(values):
+            return [cast({'key': value}, {'key': item})['key']
+                    for value, item in zip(values, items)]
+    return values
+
+
+def _cast_dict(value, definition):
+    if isinstance(value, dict):
+        schema = definition.get('schema')
+        if schema and isinstance(schema, dict):
+            return cast(value, schema)
+    return value
+
+
+def _cast_integer(value, definition):
+    if isinstance(value, basestring):
+        try:
+            return int(value)
+        except ValueError:
+            pass
+    return value
+
+
+def _cast_boolean(value, definition):
+    if isinstance(value, basestring):
+        if value.lower() in ('true', 'yes', 'on', '1'):
+            return True
+        elif value.lower() in ('false', 'no', 'off', '0'):
+            return False
+    return value
+
+
+# Todo: We'd like to get rid of the `current_app`. Unfortunately, it's also
+#     not really attractive to pass the app as extra argument to all cast
+#     functions.
+def _cast_annotation(value, definition):
+    if isinstance(value, int):
+        return Annotation.query.get(value)
+    elif isinstance(value, basestring):
+        return annotation_by_uri(current_app, value)
+    return value
+
+
+def _cast_coverage(value, definition):
+    if isinstance(value, int):
+        return Coverage.query.get(value)
+    elif isinstance(value, basestring):
+        return coverage_by_uri(current_app, value)
+    return value
+
+
+def _cast_data_source(value, definition):
+    if isinstance(value, int):
+        return DataSource.query.get(value)
+    elif isinstance(value, basestring):
+        return data_source_by_uri(current_app, value)
+    return value
+
+
+def _cast_sample(value, definition):
+    if isinstance(value, int):
+        return Sample.query.get(value)
+    elif isinstance(value, basestring):
+        return sample_by_uri(current_app, value)
+    return value
+
+
+def _cast_user(value, definition):
+    if isinstance(value, int):
+        return User.query.get(value)
+    elif isinstance(value, basestring):
+        return user_by_uri(current_app, value)
+    return value
+
+
+def _cast_variant(value, definition):
+    if isinstance(value, basestring) and len(value) <= 500:
+        # Todo: I'm not so sure about the urllib.unquote call. I would've
+        #     hoped that Werkzeug routing would unquote any argument in our
+        #     url (e.g. '/variants/<str:variant>' in views.variant_get.
+        match = re.match('([^:]+):([0-9]+)([a-zA-Z]{,200})>([a-zA-Z]{,200}$)',
+                         urllib.unquote(value))
+        try:
+            chromosome, position, reference, observed = match.groups()
+            return chromosome, int(position), reference, observed
+        except AttributeError:
+            pass
+    return value
+
+
+def _cast_variation(value, definition):
+    if isinstance(value, int):
+        return Variation.query.get(value)
+    elif isinstance(value, basestring):
+        return variation_by_uri(current_app, value)
+    return value
+
+
 # Todo: Instead of the light-weight Cerberus, I'm tempted to use the
 #     `Colander <https://github.com/Pylons/colander>` library. My main problem
 #     with it is that schema definitions are very verbose, not suitable as
@@ -50,139 +191,52 @@ ERROR_LABEL_CHARACTERS = "field '%s' of %s type must contain only uppercase alph
 #     Another alternative would be the `Voluptuous <https://github.com/alecthomas/voluptuous>`
 #     library.
 class ApiValidator(Validator):
-    # Because of the casting we do in some validators (described above), we
-    # need to aggregate values from temporary subvalidators back into the
-    # original validator. For example, in the _validate_schema rule for lists.
-    def _validate_schema(self, schema, field, value):
-        if isinstance(value, list):
-            self.document[field] = []
-            for i in range(len(value)):
-                key = "%s[%s]" % (field, str(i))
-                validator = self.__class__({key: schema})
-                if not validator.validate({key: value[i]}):
-                    self._error([error for error in validator.errors])
-                self.document[field].append(validator.document[key])
-        elif isinstance(value, dict):
-            validator = self.__class__(schema)
-            if not validator.validate(value):
-                self._error(["'%s': " % field + error
-                            for error in validator.errors])
-        else:
-            self._error(errors.ERROR_BAD_TYPE % (field, "dict"))
-
-    # Because of the casting we do in some validators (described above), we
-    # need to aggregate values from temporary subvalidators back into the
-    # original validator. For example, in the _validate_items_list rule.
-    def _validate_items_list(self, schema, field, values):
-        if len(schema) != len(values):
-            self._error(errors.ERROR_ITEMS_LIST % (field, len(schema)))
-        else:
-            self.document[field] = []
-            for i in range(len(schema)):
-                key = "%s[%s]" % (field, str(i))
-                validator = self.__class__({key: schema[i]})
-                if not validator.validate({key: values[i]}):
-                    self._error([error for error in validator.errors])
-                self.document[field].append(validator.document[key])
-
     def _validate_safe(self, safe, field, value):
         expression = '[a-zA-Z][a-zA-Z0-9._-]*$'
         if safe and not re.match(expression, value):
             self._error("value for field '%s' must match the expression '%s'"
                         % (field, expression))
 
-    def _validate_type_list(self, field, value):
-        if isinstance(value, basestring):
-            if value:
-                self.document[field] = value.split(',')
-            else:
-                self.document[field] = []
-        super(ApiValidator, self)._validate_type_list(field,
-                                                      self.document[field])
-
-    def _validate_type_integer(self, field, value):
-        if isinstance(value, basestring):
-            try:
-                self.document[field] = int(value)
-            except ValueError:
-                pass
-        super(ApiValidator, self)._validate_type_integer(field,
-                                                         self.document[field])
-
-    def _validate_type_boolean(self, field, value):
-        if isinstance(value, basestring):
-            if value.lower() in ('true', 'yes', 'on', '1'):
-                self.document[field] = True
-            elif value.lower() in ('false', 'no', 'off', '0'):
-                self.document[field] = False
-        super(ApiValidator, self)._validate_type_boolean(field,
-                                                         self.document[field])
-
-    def _validate_type_user(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = User.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = user_by_uri(current_app, value)
-        if not isinstance(self.document[field], User):
-            self._error(ERROR_BAD_TYPE % (field, 'user'))
-
-    def _validate_type_sample(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = Sample.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = sample_by_uri(current_app, value)
-        if not isinstance(self.document[field], Sample):
-            self._error(ERROR_BAD_TYPE % (field, 'sample'))
-
-    def _validate_type_variation(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = Variation.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = variation_by_uri(current_app, value)
-        if not isinstance(self.document[field], Variation):
-            self._error(ERROR_BAD_TYPE % (field, 'variation'))
+    def _validate_type_annotation(self, field, value):
+        if not isinstance(self.document[field], Annotation):
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'annotation'))
 
     def _validate_type_coverage(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = Coverage.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = coverage_by_uri(current_app, value)
         if not isinstance(self.document[field], Coverage):
-            self._error(ERROR_BAD_TYPE % (field, 'coverage'))
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'coverage'))
 
     def _validate_type_data_source(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = DataSource.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = data_source_by_uri(current_app, value)
         if not isinstance(self.document[field], DataSource):
-            self._error(ERROR_BAD_TYPE % (field, 'data_source'))
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'data_source'))
 
-    def _validate_type_annotation(self, field, value):
-        if isinstance(value, int):
-            self.document[field] = Annotation.query.get(value)
-        elif isinstance(value, basestring):
-            self.document[field] = annotation_by_uri(current_app, value)
-        if not isinstance(self.document[field], Annotation):
-            self._error(ERROR_BAD_TYPE % (field, 'annotation'))
+    def _validate_type_sample(self, field, value):
+        if not isinstance(self.document[field], Sample):
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'sample'))
+
+    def _validate_type_user(self, field, value):
+        if not isinstance(self.document[field], User):
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'user'))
 
     def _validate_type_variant(self, field, value):
-        if not isinstance(value, basestring):
-            self._error(ERROR_BAD_TYPE % (field, 'variant'))
-            return
-        if len(value) > 500:
-            self._error(ERROR_BAD_TYPE % (field, 'variant'))
-            return
-        # Todo: I'm not so sure about the urllib.unquote call. I would've
-        #     hoped that Werkzeug routing would unquote any argument in our
-        #     url (e.g. '/variants/<str:variant>' in views.variant_get.
-        match = re.match('([^:]+):([0-9]+)([a-zA-Z]{,200})>([a-zA-Z]{,200}$)',
-                         urllib.unquote(value))
-        try:
-            chromosome, position, reference, observed = match.groups()
-            self.document[field] = chromosome, int(position), reference, observed
-        except AttributeError:
-            self._error(ERROR_BAD_TYPE % (field, 'variant'))
+        if isinstance(value, tuple):
+            try:
+                chromosome, position, reference, observed = value
+                if (all(isinstance(x, basestring)
+                        for x in (chromosome, reference, observed))
+                    and isinstance(position, int)):
+                    # Todo: Must we check here if we're dealing with DNA?
+                    #     Also, these length checks are quire arbitrary.
+                    if (len(chromosome) < 100 and
+                        len(reference) < 300 and
+                        len(observed) < 300):
+                        return
+            except ValueError:
+                pass
+        self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'variant'))
+
+    def _validate_type_variation(self, field, value):
+        if not isinstance(self.document[field], Variation):
+            self._error(cerberus.errors.ERROR_BAD_TYPE % (field, 'variation'))
 
 
 def data(**schema):
@@ -212,8 +266,9 @@ def data(**schema):
         @wraps(rule)
         def data_rule(*args, **kwargs):
             # Todo: Look into Flask's `request.on_json_loading_failed`.
-            data = deepcopy(request.json) or request.values.to_dict()
-            data.update(**kwargs)
+            raw_data = request.json or request.values.to_dict()
+            raw_data.update(**kwargs)
+            data = cast(raw_data, schema)
             try:
                 if not validator.validate(data):
                     raise ValidationError('Invalid request content: %s'
