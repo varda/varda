f5652ea94e54767995213712199d3d7844ca9a95
Martijn Vermaat <martijn@vermaat.name>
2013-06-16 22:52:50 +0200

    Add unit tests for import_coverage task
    Implemented with some fixtures using the fixture package which
    unfortunately needs some monkey patching for now (see the top of
    tests/fixtures.py).

---
+++ tests/fixtures.py
@@ -0,0 +1,71 @@
+"""
+Some database fixtures for our unit tests.
+"""
+
+
+from fixture import DataSet
+
+
+def monkey_patch_fixture():
+    """
+    Fix fixture to support SQLAlchemy declarative model.
+
+    SQLAlchemy declarative model definitions with custom constructor methods
+    are note supported by fixture. Awaiting the pull request we monkey-patch
+    fixture.
+
+    Pull request: https://github.com/fixture-py/fixture/pull/2
+
+    Call this function before importing anything from the fixture package. For
+    example, start your file with the following:
+
+        import fixtures; fixtures.monkey_patch_fixture()
+        from fixture import DataSet, SQLAlchemyFixture
+    """
+    from fixture.loadable.sqlalchemy_loadable import MappedClassMedium
+
+    def save(self, row, column_vals):
+        """
+        Save a new object to the session if it doesn't already exist in the
+        session.
+        """
+        obj = self.medium(**dict(column_vals))
+        if obj not in self.session.new:
+            if hasattr(self.session, 'add'):
+                # sqlalchemy 0.5.2+
+                self.session.add(obj)
+            else:
+                self.session.save(obj)
+        return obj
+
+    MappedClassMedium.save = save
+
+
+class UserData(DataSet):
+    class test_user:
+        name = 'Test User'
+        login = 'test_login'
+        password = 'test_password'
+
+
+class SampleData(DataSet):
+    class unactivated_sample:
+        user = UserData.test_user
+        name = 'Test Sample'
+
+
+class DataSourceData(DataSet):
+    class exome_samtools_coverage:
+        user = UserData.test_user
+        name = 'Test Data Source'
+        filetype = 'bed'
+        local_file = 'exome-samtools.bed'
+
+
+class CoverageData(DataSet):
+    class unimported_exome_samtools_coverage:
+        sample = SampleData.unactivated_sample
+        data_source = DataSourceData.exome_samtools_coverage
+    class unimported_exome_samtools_coverage_2:
+        sample = SampleData.unactivated_sample
+        data_source = DataSourceData.exome_samtools_coverage
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -3,19 +3,27 @@ Test Celery tasks.
 """
 
 
+import os
 import tempfile
 
+import fixtures; fixtures.monkey_patch_fixture()
+from fixture import SQLAlchemyFixture
+from fixture.style import NamedDataStyle
 from flask.ext.testing import TestCase
 from nose.tools import *
+from sqlalchemy import create_engine
 
-from varda import create_app, db
-from varda.models import User
-from varda.tasks import ping
+from varda import create_app, db, models
+from varda.models import Coverage, Region, User
+from varda import tasks
+
+from fixtures import CoverageData
 
 
 TEST_SETTINGS = {
     'TESTING': True,
     'DATA_DIR': tempfile.mkdtemp(),
+    'SECONDARY_DATA_DIR': os.path.join(os.path.dirname(__file__), 'data'),
     'GENOME': 'tests/data/hg19.fa',
     'REFERENCE_MISMATCH_ABORT': True,
     'SQLALCHEMY_DATABASE_URI': 'sqlite://',
@@ -45,6 +53,7 @@ class TestTasks(TestCase):
         Run once before every test. Setup the test database.
         """
         db.create_all()
+        self.fixture = SQLAlchemyFixture(env=models, style=NamedDataStyle(), engine=db.engine)
 
     def tearDown(self):
         """
@@ -57,7 +66,7 @@ class TestTasks(TestCase):
         """
         Synchronously execute a task and get the result.
         """
-        assert_equal(ping.apply().result, 'pong')
+        assert_equal(tasks.ping.apply().result, 'pong')
 
     def test_ping_delayed(self):
         """
@@ -66,4 +75,56 @@ class TestTasks(TestCase):
         This works, because we set CELERY_ALWAYS_EAGER to True in the test
         environment.
         """
-        assert_equal(ping.delay().result, 'pong')
+        assert_equal(tasks.ping.delay().result, 'pong')
+
+    def test_import_coverage(self):
+        """
+        Import a coverage file.
+        """
+        with self.fixture.data(CoverageData) as data:
+            coverage = Coverage.query.get(
+                data.CoverageData.unimported_exome_samtools_coverage.id)
+            result = tasks.import_coverage.delay(coverage.id)
+            assert_equal(result.state, 'SUCCESS')
+            assert coverage.task_done
+            assert_equal(Region.query.filter_by(coverage=coverage).count(), 22)
+
+    def test_import_nonexisting_coverage(self):
+        """
+        Import a coverage file for nonexisting coverage resource.
+        """
+        with assert_raises(tasks.TaskError) as cm:
+            tasks.import_coverage.delay(27)
+        assert_equal(cm.exception.code, 'coverage_not_found')
+
+    def test_import_imported_coverage(self):
+        """
+        Import already imported coverage.
+        """
+        with self.fixture.data(CoverageData) as data:
+            coverage = Coverage.query.get(
+                data.CoverageData.unimported_exome_samtools_coverage.id)
+            result = tasks.import_coverage.delay(coverage.id)
+            assert_equal(result.state, 'SUCCESS')
+
+            with assert_raises(tasks.TaskError) as cm:
+                tasks.import_coverage.delay(coverage.id)
+            assert_equal(cm.exception.code, 'coverage_imported')
+            assert coverage.task_done
+
+    def test_import_coverage_duplicate(self):
+        """
+        Import the same coverage file twice.
+        """
+        with self.fixture.data(CoverageData) as data:
+            coverage = Coverage.query.get(
+                data.CoverageData.unimported_exome_samtools_coverage.id)
+            result = tasks.import_coverage.delay(coverage.id)
+            assert_equal(result.state, 'SUCCESS')
+
+            coverage = Coverage.query.get(
+                data.CoverageData.unimported_exome_samtools_coverage_2.id)
+            with assert_raises(tasks.TaskError) as cm:
+                tasks.import_coverage.delay(coverage.id)
+            assert_equal(cm.exception.code, 'duplicate_data_source')
+            assert not coverage.task_done
