620dab43dd58dc148a02a04a4b10e150aedc1059
Martijn Vermaat <martijn@vermaat.name>
2013-08-14 13:54:49 +0200

    Use original scheme/host/path behind reverse proxy

--- varda/__init__.py
+++ varda/__init__.py
@@ -42,6 +42,41 @@ celery = Celery('varda')
 genome = Genome()
 
 
+class ReverseProxied(object):
+    """
+    Wrap the application in this middleware and configure the front-end server
+    to add these headers, to let you quietly bind this to a URL other than /
+    and to an HTTP scheme that is different than what is used locally.
+
+    Example for nginx::
+
+        location /myprefix {
+            proxy_pass http://192.168.0.1:5001;
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Scheme $scheme;
+            proxy_set_header X-Script-Name /myprefix;
+        }
+
+    `Flask Snippet <http://flask.pocoo.org/snippets/35/>`_ from Peter Hansen.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 def create_app(settings=None):
     """
     Create a Flask instance for Varda. Configuration settings are read from a
@@ -74,4 +109,15 @@ def create_app(settings=None):
         from .web import web
         app.register_blueprint(web,
                                url_prefix=app.config['AULE_URL_PREFIX'])
+    return app
+
+
+def create_reverse_proxied_app(settings=None):
+    """
+    Create a Flask instance for Varda running behind a reverse proxy.
+
+    See :func:`create_app` and :class:`ReverseProxied`.
+    """
+    app = create_app(settings=settings)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     return app
