18c249d289bb9f814a02319228fba0d63c8047a9
Martijn Vermaat <martijn@vermaat.name>
2015-05-07 14:26:48 +0200

    Fix typo
    Thanks @sndrtj

--- varda/api/resources/samples.py
+++ varda/api/resources/samples.py
@@ -188,4 +188,4 @@ class SamplesResource(ModelResource):
         """
         Todo: documentation, including how/if we cascade.
         """
-        return super(SamplersResource, cls).delete_view(*args, **kwargs)
+        return super(SamplesResource, cls).delete_view(*args, **kwargs)
